c8ed376dec38dd848ef5cc4343329d8b36f443ef
Miss-Rey <audreychelangat2003@gmail.com>
2023-10-04 15:03:48 -0700

    updated

---
+++ create_static_lib.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
+
+create_static_lib.sh
+
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
+
